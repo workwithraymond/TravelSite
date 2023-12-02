05d442702e5f54e1f2085ab832c7962e949c0d28
workwithraymond <workwithraymond01@gmail.com>
2023-12-02 13:23:25 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import About from './components/About';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import Tours from './components/Tours';
+
+
+function App() {
+  return (
+    <>
+   
+<Navbar />
+<Hero /> 
+<About />
+<Services />
+<Tours />
+<Footer />
+ 
+    </>
+  );
+}
+
+export default App;
